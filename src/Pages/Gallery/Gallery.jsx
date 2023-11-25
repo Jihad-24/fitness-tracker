bc0ceb1fb3fbfcf23bba81ee1dfa1b7c260b64df
Jihad Hasan <mdjihat0021@gmail.com>
2023-11-25 16:53:57 +0600

    finished gallary page

--- src/Pages/Gallery/Gallery.jsx
+++ src/Pages/Gallery/Gallery.jsx
@@ -1,9 +1,106 @@
-
+import { useState, useEffect, useCallback } from 'react';
 
 const Gallery = () => {
+    const [images, setImages] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [page, setPage] = useState(1);
+    const perPage = 12;
+
+    const fetchImages = useCallback(async () => {
+        setIsLoading(true);
+        await new Promise(resolve => setTimeout(resolve, 1000));
+
+        const newImages = Array.from({ length: perPage }, (_, index) => ({
+            id: (page - 1) * perPage + index + 1,
+            url: `https://source.unsplash.com/random/400x400?sig=${(page - 1) * perPage + index}`,
+        }));
+
+        setIsLoading(false);
+        setImages(prevImages => [...prevImages, ...newImages]);
+    }, [page, perPage]);
+
+    const handleScroll = useCallback(() => {
+        const { scrollTop, clientHeight, scrollHeight } = document.documentElement;
+
+        if (scrollTop + clientHeight >= scrollHeight - 20 && !isLoading) {
+            setPage(prevPage => prevPage + 1);
+        }
+    }, [isLoading]);
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, [handleScroll]);
+
+    useEffect(() => {
+        if (images.length < perPage) {
+            fetchImages();
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    useEffect(() => {
+        if (page > 1) {
+            fetchImages();
+        }
+    }, [page, fetchImages]);
+
     return (
-        <div>
-            gallery
+        <div className="mx-5 md:mx-10 my-12">
+            <div className="gallery">
+                <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
+                    {(isLoading ? Array.from({ length: perPage }, (_, index) => (
+                        <div key={index} className="grid-item">
+                            <div className="flex flex-col gap-4 w-52">
+                                <div className="skeleton h-32 w-full"></div>
+                                <div className="skeleton h-4 w-28"></div>
+                                <div className="skeleton h-4 w-full"></div>
+                                <div className="skeleton h-4 w-full"></div>
+                            </div>
+                        </div>
+                    )) : images.slice(0, page * perPage).map(image => (
+                        <div key={image.id} className="grid-item">
+                            <img
+                                src={image.url}
+                                alt={`Image ${image.id}`}
+                            />
+                        </div>
+                    )))}
+                </div>
+                {isLoading &&
+                    <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5'>
+                        <div className="flex flex-col gap-4 w-52">
+                            <div className="skeleton h-32 w-full"></div>
+                            <div className="skeleton h-4 w-28"></div>
+                            <div className="skeleton h-4 w-full"></div>
+                            <div className="skeleton h-4 w-full"></div>
+                        </div>
+                        <div className="flex flex-col gap-4 w-52">
+                            <div className="skeleton h-32 w-full"></div>
+                            <div className="skeleton h-4 w-28"></div>
+                            <div className="skeleton h-4 w-full"></div>
+                            <div className="skeleton h-4 w-full"></div>
+                        </div>
+                        <div className="hidden md:block">
+                            <div className="flex flex-col gap-4 w-52">
+                                <div className="skeleton h-32 w-full"></div>
+                                <div className="skeleton h-4 w-28"></div>
+                                <div className="skeleton h-4 w-full"></div>
+                                <div className="skeleton h-4 w-full"></div>
+                            </div>
+                        </div>
+                        <div className="hidden lg:block">
+                            <div className="flex flex-col  gap-4 w-52">
+                                <div className="skeleton h-32 w-full"></div>
+                                <div className="skeleton h-4 w-28"></div>
+                                <div className="skeleton h-4 w-full"></div>
+                                <div className="skeleton h-4 w-full"></div>
+                            </div>
+                        </div>
+                    </div>}
+            </div>
         </div>
     );
 };
